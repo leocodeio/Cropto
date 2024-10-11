d036358c729f85ead85f3da21440c7c4c56a345f
leodoeio <basicleo787@gmail.com>
2024-10-11 21:28:09 +0530

    updated and brought up running

--- app/data.server.tsx
+++ app/data.server.tsx
@@ -1,16 +1,39 @@
 export async function getPestisides() {
-  const dummyResponse = ["Pesticide 1", "Pesticide 2", "Pesticide 3"];
-  const response = dummyResponse;
-  return response;
+  // console.log(process.env.ML_MODEL_URL);
+  const response = await fetch(`${process.env.ML_MODEL_URL}/pesticides`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const pesticides = await response.json();
+  return pesticides;
 }
 
 export async function predictCropSuccess(payload: {
-  rainfall: string;
+  rainfall: number;
   crop: string;
   soil_type: string;
   season: string;
   pesticide: string;
 }) {
-  console.log(payload);
-  return 100;
+  const { rainfall, crop, soil_type, season, pesticide } = payload;
+  //   console.log(payload);
+  const response = await fetch(`${process.env.ML_MODEL_URL}/predict`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      rainfall: rainfall,
+      crop: crop,
+      soil_type: soil_type,
+      season: season,
+      pesticide: pesticide,
+    }),
+  });
+
+  const prediction_value = await response.json();
+  //   console.log(prediction_value);
+  return prediction_value["success_rate"] || 0;
 }
